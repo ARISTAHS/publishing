3532ab1b223cf33c9e42ec72a334b2b8ed0bf290
JHS <118969653+ARISTAHS@users.noreply.github.com>
2023-11-29 14:50:41 +0900

    고객리스트 js 진행
    js 추가 작업 진행

--- study/customerList/js/main.js
+++ study/customerList/js/main.js
@@ -1,6 +1,4 @@
 // 페이지 상단 이동
-const body = document.querySelector('body');
-const main = document.querySelector('body main');
 const topBtn = document.querySelector('#footerArea .topMove');
 
 const handleScroll = () =>{
@@ -11,6 +9,10 @@ const handleScroll = () =>{
     topBtn.style.opacity='0';
   }
 };
+
+handleScroll(); //초기 상태 설정
+window.addEventListener('scroll', handleScroll);
+
 topBtn.onclick = (e) =>{ 
   e.preventDefault();
   window.scrollTo({
@@ -19,5 +21,57 @@ topBtn.onclick = (e) =>{
   });
 };
 
-handleScroll(); //초기 상태 설정
-window.addEventListener('scroll', handleScroll);
+
+// nav 영역 
+const menuBtn = document.querySelector('.sideNav .menu');
+const sideArea = document.querySelector('.sideNav #nav-total');
+const body = document.querySelector('body');
+
+function sideMove(){
+  // 메뉴 버튼을 눌렀을때 nav 영역 동작 
+  if(sideArea.style.display === 'none'){
+    sideArea.style.display = 'block';
+    document.body.style.gridTemplateColumns = '250px 1fr';
+
+  }else{
+    sideArea.style.display = 'none';
+    document.body.style.gridTemplateColumns = '60px 1fr';
+  };
+}
+menuBtn.onclick = sideMove ; // 버튼을 클릭하면 숨기기, 다시 클릭하면 노출
+
+function clickNav(){
+  const sideNav = document.querySelector('.sideNav-icon .icon-list button');
+  sideNav.classList.toggle('on'); //아이콘에 on 클래스 추가 
+}
+function hideAllAreas(){
+  const areas = document.querySelectorAll('.sideNav-area > li');
+  const btns = document.querySelectorAll('.icon-list > button');
+
+  areas.forEach(area => {
+    area.style.display='none';
+  });
+
+  btns.forEach(button=>{
+    button.classList.remove('on');
+  });
+}
+function iconClick(clickedBtn) {
+  const targetId = clickedBtn.id.replace('Btn', '');  // 'Btn' 부분을 삭제하여 ID 얻기
+  const targetArea = document.getElementById(targetId);
+
+  if (targetArea) {
+    if (targetArea.style.display === 'block') {
+      // 이미 보이는 상태이면 숨기고 on 클래스 제거
+      targetArea.style.display = 'none';
+      clickedBtn.classList.remove('on');
+    } else {
+      // 보이지 않는 상태이면 보이게 하고 on 클래스 추가
+      hideAllAreas();
+      targetArea.style.display = 'block';
+      clickedBtn.classList.add('on');
+    }
+  } else {
+    console.error(`Element with ID '${targetId}' not found.`);
+  }
+}
