0e43b7af512e2db210d36df6e0c83a8e3494b19d
ARISTAHS <paran0907@naver.com>
2024-07-26 20:41:03 +0900

    input 값 확인

--- todoList/main.js
+++ todoList/main.js
@@ -1,3 +1,12 @@
-function keyCodeCheck(){
-  console.log(keyCodeCheck);
+const todoForm = document.getElementById('todoForm');
+const list = document.getElementById('todoList');
+const todoInput = document.getElementById('todo');
+
+
+function handleTodo(event){
+  event.preventDefault();
+
+  console.log(todoInput.value);
 }
+
+todoForm.addEventListener('submit', handleTodo);
