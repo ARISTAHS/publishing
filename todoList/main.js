e756b21c5a76b4bfb6556be00e4785e9c3099d8a
ARISTAHS <paran0907@naver.com>
2024-07-30 20:38:49 +0900

    스크립트 기능구현 1

--- todoList/main.js
+++ todoList/main.js
@@ -1,12 +1,33 @@
 const todoForm = document.getElementById('todoForm');
 const list = document.getElementById('todoList');
-const todoInput = document.getElementById('todo');
+let todoInput = document.getElementById('todo');
+
+//목록 추가
+function addTodo(){
+
+  if(todoInput.value == false){
+    alert('할일을 입력하세요');
+  }else{
+    let result = document.createElement('li');
+    console.log(result);
+    let delBtn = document.createElement('button');
+
+    result.innerHTML = todoInput.value;
+    list.appendChild(result);
+
+    result.appendChild(delBtn);
+    console.log(delBtn);
+    delBtn.innerText = '🙌';
+    delBtn.addEventListener('click', delTodo);
+    
+  }
+  
 
 
-function handleTodo(event){
-  event.preventDefault();
-
-  console.log(todoInput.value);
 }
 
-todoForm.addEventListener('submit', handleTodo);
+// 목록 삭제 버튼
+function delTodo(e){
+  let removeList = e.target.parentElement;
+  removeList.remove();
+}
