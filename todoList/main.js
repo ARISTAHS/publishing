c4b737c611afdf0790eddc97f9e70345b84a41ca
ARISTAHS <paran0907@naver.com>
2024-07-30 21:26:05 +0900

    onkeydown 방식 추가

--- todoList/main.js
+++ todoList/main.js
@@ -36,3 +36,22 @@ function delTodo(e){
   let removeList = e.target.parentElement;
   removeList.remove();
 }
+
+
+//다른 방식 추가 (input에 목록 입력후 엔터를 누를 경우)
+function keyCodeCheck(){
+  console.log(window.event);
+  if(window.event.keyCode === 13 && todoInput.value !== ''){ //if 조건 두개 연결
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newBtn = document.createElement('button');
+
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan);
+
+    newSpan.textContent = todoInput.value;
+
+    list.appendChild(newLi);
+    todoInput.value = ''; //list에 입력 후 input 초기화
+  }
+}
